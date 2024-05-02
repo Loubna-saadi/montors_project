909265c459d2215058585cd011422ff465a10c95
loubna saadi <loubnasaadi88@gmail.com>
2024-05-02 21:10:37 +0100

    login-done

--- server/controllers/ApiController.js
+++ server/controllers/ApiController.js
@@ -13,28 +13,31 @@ const generateToken = (user) => {
 };
 
 const apiControllers = {
+
   register: async (req, res) => {
     try {
-    //  const salt=await bcrypt.genSalt(10)
-    //  const hashedPassword=await bcrypt.hash(req.body.password,salt)
       const { email, phone, username, password, role } = req.body;
-
+  
       // Check if user with the same email exists
       const existingUser = await User.findOne({ email });
       if (existingUser) {
         return res.status(400).json({ message: 'Email already exists' });
       }
-
-      // Create a new user
-      const newUser = new User({ email, phone, username, password, role });
+  
+      // Hash the password before saving the user
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      // Create a new user with hashed password
+      const newUser = new User({ email, phone, username, password: hashedPassword, role });
       await newUser.save();
-
+  
       res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: 'Server error' });
     }
   },
+  
 
   login: async (req, res) => {
     try {
--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['mentor', 'client'], default: 'client' },
-  
+  role: { type: String, enum: ['mentor', 'client'], default: 'client' }
 });
+
+// Define comparePassword method
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 const User = mongoose.model('User', userSchema);
 
